a54b0c9f18a0ff9fae55905dace9077dd304641a
Zhoobin Daghooghi <daghooghi@gmail.com>
2024-10-26 09:12:36 +0330

    End of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+  const [activties, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/Activities')
+      .then(response => {
+        console.log(response)
+        setActivities(response.data)
+      }
+      )
+  }, [])
+
+  return (
+    <div>
+      <Header as='h2' icon='users' content='Reactivities' />
+      <List>
+        {activties.map((activty: any) => (
+          <List.Item key={activty.id}>
+            {activty.title}
+          </List.Item>
+        ))}
+      </List>
+    </div>
+
+  )
+}
+
+export default App
